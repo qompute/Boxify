1df147dcce15d7c5dcfc7490ed14b7fd56d23037
Qinhong Chen <qinhong.chen@berkeley.edu>
2019-10-26 02:59:29 -0700

    created camera preview in main activity

--- app/src/main/java/com/github/qompute/boxify/MainActivity.java
+++ app/src/main/java/com/github/qompute/boxify/MainActivity.java
@@ -2,13 +2,49 @@ package com.github.qompute.boxify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Camera camera;
+    private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera = getCameraInstance();
+        camera.setDisplayOrientation(90);
+        cameraPreview = new CameraPreview(this, camera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(cameraPreview);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    private static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            e.printStackTrace();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void releaseCamera(){
+        if (camera != null){
+            camera.release(); // release the camera for other applications
+            camera = null;
+        }
     }
 }
