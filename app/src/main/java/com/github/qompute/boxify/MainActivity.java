54008c31e3e218a545bcc300a4cdec2429bc0043
Qinhong Chen <qinhong.chen@berkeley.edu>
2019-10-26 04:38:59 -0700

    changed Camera to new CameraX library

--- app/src/main/java/com/github/qompute/boxify/MainActivity.java
+++ app/src/main/java/com/github/qompute/boxify/MainActivity.java
@@ -1,50 +1,105 @@
 package com.github.qompute.boxify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.hardware.Camera;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-    private Camera camera;
-    private CameraPreview cameraPreview;
+    private int REQUEST_CODE_PERMISSIONS = 10;
+    private String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA};
+    private TextureView viewFinder;
+    private Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera = getCameraInstance();
-        camera.setDisplayOrientation(90);
-        cameraPreview = new CameraPreview(this, camera);
-        FrameLayout preview = findViewById(R.id.camera_preview);
-        preview.addView(cameraPreview);
+        viewFinder = findViewById(R.id.view_finder);
+        if (allPermissionsGranted()) {
+            viewFinder.post(this::startCamera);
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
+        viewFinder.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) ->
+            updateTransform());
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        releaseCamera();
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    private static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-            e.printStackTrace();
-        }
-        return c; // returns null if camera is unavailable
+    private void startCamera() {
+        PreviewConfig previewConfig = new PreviewConfig.Builder().build();
+        Preview preview = new Preview(previewConfig);
+
+        preview.setOnPreviewOutputUpdateListener(output -> {
+            ViewGroup parent = (ViewGroup) viewFinder.getParent();
+            parent.removeView(viewFinder);
+            parent.addView(viewFinder, 0);
+
+            viewFinder.setSurfaceTexture(output.getSurfaceTexture());
+            updateTransform();
+        });
+        CameraX.bindToLifecycle(this, preview);
     }
 
-    private void releaseCamera(){
-        if (camera != null){
-            camera.release(); // release the camera for other applications
-            camera = null;
+    private void updateTransform() {
+        Matrix matrix = new Matrix();
+
+        float centerX = viewFinder.getWidth() / 2f;
+        float centerY = viewFinder.getHeight() / 2f;
+
+        float rotationDegrees = 0f;
+        switch (viewFinder.getDisplay().getRotation()) {
+            case Surface.ROTATION_0: rotationDegrees = 0f; break;
+            case Surface.ROTATION_90: rotationDegrees = 90f; break;
+            case Surface.ROTATION_180: rotationDegrees = 180f; break;
+            case Surface.ROTATION_270: rotationDegrees = 270f; break;
+            default: return;
         }
+        matrix.postRotate(-rotationDegrees, centerX, centerY);
+        viewFinder.setTransform(matrix);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post(this::startCamera);
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String p : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(getBaseContext(), p) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
